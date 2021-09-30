0fa0c1aeb98cacf227ec1c4fd1f3a4840ae1737e
laurasalet <laurasilvasalet@gmail.com>
2021-09-29 21:26:06 -0400

    first commit

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -6,17 +6,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-  numero1: number=0;
-  numero2: number=0;
-  resultado: number=0;
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
-add(): void {
-   this.resultado = this.numero1 / this.numero2;
-   }
+
 
 }
